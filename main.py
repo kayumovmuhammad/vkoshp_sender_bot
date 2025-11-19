d106466ec167b6d01b262c4915fba9ff6e544937
Muhammad <kayumovmuhamma@gmail.com>
2025-11-19 11:54:51 +0500

    first commit

---
+++ main.py
@@ -0,0 +1,42 @@
+from time import sleep
+from dotenv import load_dotenv
+from os import getenv
+import requests
+
+load_dotenv()
+
+timer = 0
+
+BOT_TOKEN = getenv("BOT_TOKEN")
+URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+# WEBSITE_URL = "https://neerc.ifmo.ru/school/russia-team/"
+WEBSITE_URL = "https://kayumovmuhammad.github.io/"
+ADMIN_ID = getenv("KAYUMOVMUHAMMAD_ID")
+USERS_ID = [ADMIN_ID, getenv("MAHDI_ID")]
+
+print(ADMIN_ID)
+
+def send_message(chat_id, message):
+    data = {
+        "chat_id": chat_id,
+        "text": message
+    }
+    response = requests.post(URL, data=data)
+    return response
+
+oldHTML = requests.get(WEBSITE_URL).text
+
+while True:
+    if timer % 12 == 0:
+        send_message(ADMIN_ID, "Все работает✅")
+    timer += 1
+
+    newHTML = requests.get(WEBSITE_URL).text
+    
+    if (newHTML != oldHTML):
+        for chat_id in USERS_ID:
+            send_message(chat_id, f"Что то изменилось на сайте: {WEBSITE_URL}")
+        oldHTML = newHTML
+
+    sleep(10)
