ccf06b20a071e08c938083f31f2fc6feeb576863
Muhammad <kayumovmuhamma@gmail.com>
2025-11-21 22:58:20 +0500

    add: alena

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
-from time import sleep
-from dotenv import load_dotenv
 from os import getenv
+from time import sleep
+
 import requests
+from dotenv import load_dotenv
 
 load_dotenv()
 
@@ -10,32 +11,46 @@ timer = 0
 BOT_TOKEN = getenv("BOT_TOKEN")
 URL = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
 
-WEBSITE_URL = "https://neerc.ifmo.ru/school/russia-team/"
+WEBSITE_URLS = [
+    "https://neerc.ifmo.ru/school/russia-team/",
+    "https://neerc.ifmo.ru/school/archive/2025-2026/ru-olymp-team-internet-2025-standings.html",
+    "https://neerc.ifmo.ru/school/information/index.html",
+    "https://neerc.ifmo.ru/school/archive/2025-2026.html",
+]
 ADMIN_ID = getenv("KAYUMOVMUHAMMAD_ID")
-USERS_ID = [ADMIN_ID, getenv("MAHDI_ID"), getenv("AKAI_SUHROB_ID")]
-print(ADMIN_ID)
+USERS_ID = [ADMIN_ID, 1982459265, 1351195326, 775472748]
+
 
 def send_message(chat_id, message):
-    data = {
-        "chat_id": chat_id,
-        "text": message
-    }
+    data = {"chat_id": chat_id, "text": message}
     response = requests.post(URL, data=data)
     print(response.text, chat_id)
     return response
 
-oldHTML = requests.get(WEBSITE_URL).text
+
+def logger(message: str):
+    print(f"LOGGER: {message}")
+
+
+oldHTMLs = []
+for url in WEBSITE_URLS:
+    oldHTMLs.append(requests.get(url).text)
+
+logger("Start polling")
 
 while True:
     if timer % 12 == 0:
-        send_message(ADMIN_ID, "Все работает✅")
+        send_message(ADMIN_ID, "Все работает ✅")
     timer += 1
 
-    newHTML = requests.get(WEBSITE_URL).text
-    
-    if (newHTML != oldHTML):
-        for chat_id in USERS_ID:
-            send_message(chat_id, f"Что то изменилось на сайте: {WEBSITE_URL}")
-        oldHTML = newHTML
+    for index in range(len(oldHTMLs)):
+        newHTML = requests.get(WEBSITE_URLS[index]).text
 
-    sleep(60*10)
+        if newHTML != oldHTMLs[index]:
+            for chat_id in USERS_ID:
+                send_message(
+                    chat_id, f"Что то изменилось на сайте: {WEBSITE_URLS[index]}"
+                )
+            oldHTMLs[index] = newHTML
+
+    sleep(60)
